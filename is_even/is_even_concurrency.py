d8c825bb7e64fbcf7fde47ad7a8be87b81ef4962
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-03 11:32:10 -0500

    :memo: Docstrings for Async module

--- is_even/is_even_concurrency.py
+++ is_even/is_even_concurrency.py
@@ -13,6 +13,13 @@ API_URL = "https://api.isevenapi.xyz/api/iseven/"
 @lru_cache(maxsize=None)
 @retry(ConnectionError, tries=3, delay=2)
 async def is_even(session, number: int) -> bool:
+    """is_even.
+
+    :param session:
+    :param number:
+    :type number: int
+    :rtype: bool
+    """
     r = requests.get(API_URL + str(number))
     async with session as session:
         if r.status_code == requests.codes.ok:
@@ -22,16 +29,35 @@ async def is_even(session, number: int) -> bool:
 
 
 def is_odd(session, number: int) -> bool:
+    """is_odd.
+
+    :param session:
+    :param number:
+    :type number: int
+    :rtype: bool
+    """
     return not is_even(session, number)
 
 
 async def ad(session, number: int) -> str:
+    """ad.
+
+    :param session:
+    :param number:
+    :type number: int
+    :rtype: str
+    """
     async with session as session:
         r = requests.get(API_URL + str(number))
         return r.json()["ad"]
 
 
 async def ad_without_number(session) -> str:
+    """ad_without_number.
+
+    :param session:
+    :rtype: str
+    """
 
     async with session as session:
         r = requests.get(API_URL + str(2))
